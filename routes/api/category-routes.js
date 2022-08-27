1ce79dd9dc07f392fa5ea62f985e5d88ba8a820d
Brandon Davis <brancdavis@gmail.com>
2022-08-27 16:29:54 -0400

    category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,126 @@
 const router = require('express').Router();
+const { json } = require('express');
 const { Category, Product } = require('../../models');
+const { update } = require('../../models/Category');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({ // be sure to include its associated Products
+    include: [{model: Product}]
+  })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+  })
+  
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    include: [{model: Product}]
+  })
+    .then(categoryData => {
+      if (!categoryData) { 
+        res.status(404).json({ message: `No category found with an id of ${req.params.id}` })
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  //check in a category with this name already exists
+  const categoryTitle = req.body.category_name.charAt(0).toUpperCase() + req.body.category_name.slice(1).toLowerCase();
+
+  Category.findOne({
+    where: { category_name: categoryTitle }
+  })
+    .then(checkData => {
+      if (checkData) {
+        res.status(400).json({ message: 'This category name already exists!' });
+        return true
+      }
+      return false;
+    })
+    .then(exists => {
+
+      if (exists) {
+        return;
+      }
+
+      Category.create({ category_name: categoryTitle })
+        .then(createData => res.json(createData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      })
+    })
+
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  //ensure consistant names for titles
+  const categoryTitle = req.body.category_name.charAt(0).toUpperCase() + req.body.category_name.slice(1).toLowerCase();
+
+  Category.findOne({
+    where: { category_name: categoryTitle}
+  })
+    .then(checkData => {
+      if (checkData) {
+        res.status(400).json({ message: 'This category name already exists!' });
+        return true
+      }
+      return false;
+    })
+    .then(exists => {
+
+      if (exists) {
+        return;
+      }
+
+      Category.update({
+        category_name: categoryTitle
+      },
+        {
+        where: {id: req.params.id}
+        })
+      .then(updateData => {
+          if (updateData[0] === 0) {
+            res.status(404).json({ message: "No category exists with this id!" })
+            return;
+          }
+        res.status(200).json({ message: "The category name has been successfully updated" });
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
+  
+  //res.json({ message: categoryTitle });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  })
+    .then(destroyData => {
+      if (!destroyData) {
+        res.status(404).json({ message: "No category can be found with this id" });
+        return;
+      }
+      res.json({ message: "Category has been successfully deleted" });
+  })
 });
 
 module.exports = router;
